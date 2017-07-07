21b2785741516795282a373c6f38fc8b6bda139c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-07 08:18:20 -0400

    initial code

---
+++ src/github-post-release-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const githubPostRelease = require('.')
+
+describe('github-post-release', () => {
+  it('write this test', () => {
+    console.assert(githubPostRelease, 'should export something')
+  })
+})
