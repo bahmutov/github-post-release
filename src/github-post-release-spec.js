75a84a2d4ce50eb1e28cffac4a619896290f5f8e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-18 21:39:51 -0400

    start testing

--- src/github-post-release-spec.js
+++ src/github-post-release-spec.js
@@ -1,10 +1,29 @@
 'use strict'
 
+const la = require('lazy-ass')
+const is = require('check-more-types')
+const join = require('path').join
+const R = require('ramda')
+
 /* global describe, it */
 const githubPostRelease = require('.')
+const pkg = require(join(__dirname, '..', 'package.json'))
 
-describe('github-post-release', () => {
-  it('write this test', () => {
-    console.assert(githubPostRelease, 'should export something')
+describe.only('github-post-release', () => {
+  it('is a function', () => {
+    la(is.fn(githubPostRelease))
+  })
+
+  it('forms changelog', done => {
+    const config = {}
+    const json = R.clone(pkg)
+    json.version = '1000.0.0'
+
+    githubPostRelease(config, { pkg: json }, (err, log) => {
+      la(!err, 'there was an error', err)
+      la(is.unemptyString(log), 'missing log', log)
+      console.log(log)
+      done()
+    })
   })
 })
